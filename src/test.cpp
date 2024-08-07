f7defa129f5166a8f7e42d93100d2b35ab37b578
sappho192 <sappho192@gmail.com>
2024-08-07 20:14:33 +0900

    lean_and_mean

--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,10 @@
 // test.cpp
-#include <sqlite3.h>
-#include <stdio.h>
+#include <iostream>
 #include <fmt/core.h>
 
 // main function!
 int main()
 {
     fmt::print("test!\n");
-    printf("%s\n", sqlite3_libversion());
     return 0;
 }
