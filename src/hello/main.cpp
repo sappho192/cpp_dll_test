a1044109b298d099913c292b96111a63af5c445e
sappho192 <sappho192@gmail.com>
2024-08-11 11:16:06 +0900

    Add coost

--- src/hello/main.cpp
+++ src/hello/main.cpp
@@ -2,9 +2,29 @@
 #include <iostream>
 #include <fmt/core.h>
 
+#include "co/log.h"
+#include "co/co.h"
+
 // main function!
 int main()
 {
     fmt::print("Hello, world!\n");
+
+    WLOG << "hello" << 23;
+
+    co::wait_group wg;
+    wg.add(2);
+
+    go([wg](){
+        std::cout << "hello world" << std::endl;
+        wg.done();
+    });
+
+    go([wg](){
+        std::cout << "hello again" << std::endl;
+        wg.done();
+    });
+
+    wg.wait();
     return 0;
 }
