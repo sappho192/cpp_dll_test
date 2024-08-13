d8b6733844e90881c361fb2d03d0dae3debb377e
Taein KIM <sappho192@gmail.com>
2024-08-13 16:59:57 +0900

    Write channel example

--- src/hello/main.cpp
+++ src/hello/main.cpp
@@ -1,30 +1,85 @@
 // main.cpp
 #include <iostream>
+#include <string>
+using std::string;
 #include <fmt/core.h>
 
-#include "co/log.h"
 #include "co/co.h"
+using co::chan;
+#include "co/cout.h"
+
+void pinger(chan<string>& ch)
+{
+    for (size_t i = 0; i < 5; i++)
+    {
+        ch << "ping";
+    }
+}
+
+void ponger(chan<string>& ch)
+{
+    for (size_t i = 0; i < 5; i++)
+    {
+        ch << "pong";
+    }
+}
+
+void printer(chan<string>& ch)
+{
+    for (size_t i = 0; i < 10; i++)
+    {
+        string str;
+        ch >> str;
+        std::cout << str << std::endl;
+    }
+}
 
 // main function!
 int main()
 {
     fmt::print("Hello, world!\n");
 
-    WLOG << "hello" << 23;
+    // WLOG << "hello" << 23;
 
     co::wait_group wg;
     wg.add(2);
 
-    go([wg](){
+    go([wg]()
+       {
         std::cout << "hello world" << std::endl;
-        wg.done();
-    });
+        wg.done(); });
 
-    go([wg](){
+    go([wg]()
+       {
         std::cout << "hello again" << std::endl;
-        wg.done();
-    });
+        wg.done(); });
 
     wg.wait();
+
+    co::wait_group wg2;
+    wg2.add(3);
+
+    chan<string> ch;
+
+    go([wg2, &ch]()
+    {
+        pinger(ch);
+        wg2.done();
+    });
+
+    go([wg2, &ch]()
+    {
+        ponger(ch);
+        wg2.done();
+    });
+
+    go([wg2, &ch]()
+    {
+        printer(ch);
+        wg2.done();
+    });
+
+    wg2.wait();
+
     return 0;
 }
